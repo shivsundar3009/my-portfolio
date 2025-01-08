679439cc77701fbcf297205a133e2c68eaaa3def
shivsundar3009 <shivsundar3009@gmail.com>
2025-01-08 23:37:58 +0530

    some bug fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-scroll';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
     <nav className="bg-white dark:bg-gray-900 shadow-lg">
@@ -9,7 +15,17 @@ function Navbar() {
         <h1 className="text-2xl font-bold text-gray-900 dark:text-white">
           Portfolio
         </h1>
-        <ul className="flex space-x-6 text-gray-700 dark:text-gray-300">
+
+        {/* Hamburger Icon for smaller devices */}
+        <button
+          className="md:hidden text-gray-900 dark:text-white"
+          onClick={toggleMenu}
+        >
+          {isMenuOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+        </button>
+
+        {/* Navigation Links for medium screens and larger */}
+        <ul className="hidden md:flex space-x-6 text-gray-700 dark:text-gray-300">
           <li>
             <Link
               to="hero"
@@ -62,6 +78,69 @@ function Navbar() {
           </li>
         </ul>
       </div>
+
+      {/* Mobile Menu Panel */}
+      {isMenuOpen && (
+        <div className="md:hidden bg-white dark:bg-gray-900 p-4 shadow-lg">
+          <ul className="flex flex-row justify-evenly text-gray-900 dark:text-white">
+            <li>
+              <Link
+                to="hero"
+                smooth={true}
+                duration={500}
+                className="cursor-pointer hover:text-blue-600"
+                onClick={toggleMenu}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="about"
+                smooth={true}
+                duration={500}
+                className="cursor-pointer hover:text-blue-600"
+                onClick={toggleMenu}
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="skills"
+                smooth={true}
+                duration={500}
+                className="cursor-pointer hover:text-blue-600"
+                onClick={toggleMenu}
+              >
+                Skills
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="projects"
+                smooth={true}
+                duration={500}
+                className="cursor-pointer hover:text-blue-600"
+                onClick={toggleMenu}
+              >
+                Projects
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="contact"
+                smooth={true}
+                duration={1000}
+                className="cursor-pointer hover:text-blue-600"
+                onClick={toggleMenu}
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
     </nav>
   );
 }
